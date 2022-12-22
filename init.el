db2212b599fe93126b3ff97769888e32b051d7ec
Matthtica <zxnightstalker199733@gmail.com>
2022-12-22 07:25:02 +0630

    Saved config.org and init.el to

---
+++ init.el
@@ -0,0 +1,19 @@
+(org-babel-load-file
+ (expand-file-name "config.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("79b922b0d10d379c8d0e8db0f05f0eec98ed4a50cb7e199c97b0369d19e89d49" "b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d" "37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" "5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "3f48acc0b6cbedc3126e25eaad128f580d445654eab6602373061cb793ce58c7" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "eff66f28da7d1590650ebcc602fc1752fec11fe03ad0f01cc9696c31d7f27e7b" "b54b64550215242ad351a5c7219f1b30d2cec3d1710a2121862ae6d51c7a1c12" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" default))
+ '(org-export-backends '(ascii html icalendar latex md odt))
+ '(package-selected-packages
+   '(scss-mode hover lsp-dart svelte-mode rust-mode avy dashboard-ls saveplace-pdf-view pdf-tools web-mode ivy-prescient ivy-posframe highlight-indent-guides org-superstar base16-theme gruvbox-theme org-roam-ui magit-section magit org-roam awesome-tab smartparens tree-sitter-langs tree-sitter flycheck rainbow-mode centaur-tabs auto-complete sly helpful counsel ivy-rich ivy command-log-mode lsp-mode projectile general which-key use-package spaceline nord-theme neotree material-theme evil-leader evil-collection doom-themes dashboard beacon atom-one-dark-theme all-the-icons)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background nil :foreground nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 135 :width normal :foundry "IBM " :family "BlexMono Nerd Font")))))
